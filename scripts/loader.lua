3a9f572ad82a37cf35a1280a5b20bab1df5b1361
kdott_cam <expiredtayden@gmail.com>
2026-08-20 00:21:57 -0400

    Update loader.lua

--- scripts/loader.lua
+++ scripts/loader.lua
@@ -3,7 +3,7 @@ local scripts = {
     "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/CleanAllTheLeaves.lua",
     "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/BladeBall.lua",
     "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/SpellingBee.lua",
-    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/UntitledBoxingGame.lua",
+    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/UntiltedBoxingGame.lua",
 }
 
 for _, url in ipairs(scripts) do

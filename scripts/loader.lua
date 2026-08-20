178f7edfc8152cacf7107d4be522ac8be5aa25e8
kdott_cam <expiredtayden@gmail.com>
2026-08-20 01:29:50 -0400

    Remove script loading functionality from loader.lua

--- scripts/loader.lua
+++ scripts/loader.lua
@@ -1,11 +1,1 @@
-local scripts = {
-    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/FootballFusion.lua",
-    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/CleanAllTheLeaves.lua",
-    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/BladeBall.lua",
-    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/SpellingBee.lua",
-    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/UntiltedBoxingGame.lua",
-}
 
-for _, url in ipairs(scripts) do
-    loadstring(game:HttpGet(url))()
-end

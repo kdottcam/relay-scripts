6530eba141b958438eb09a485af0b927d1b2a4a9
kdott_cam <expiredtayden@gmail.com>
2026-08-19 23:31:19 -0400

    Add loader script to load multiple Lua scripts

---
+++ scripts/loader.lua
@@ -0,0 +1,11 @@
+local scripts = {
+    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/FootballFusion.lua",
+    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/CleanAllTheLeaves.lua",
+    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/BladeBall.lua",
+    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/SpellingBee.lua",
+    "https://raw.githubusercontent.com/kdottcam/relay-scripts/main/scripts/UntitledBoxingGame.lua",
+}
+
+for _, url in ipairs(scripts) do
+    loadstring(game:HttpGet(url))()
+end
